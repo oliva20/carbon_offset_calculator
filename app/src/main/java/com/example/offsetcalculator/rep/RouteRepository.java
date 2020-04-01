bd9b9fed23d3cab553e65b93586304719940484a
oliva20 <3oliva41@solent.ac.uk>
2020-04-01 17:34:21 +0100

    added a function to calculate the distace between cordinates in list, it hasn't been test yet though

--- app/src/main/java/com/example/offsetcalculator/rep/RouteRepository.java
+++ app/src/main/java/com/example/offsetcalculator/rep/RouteRepository.java
@@ -1,6 +1,7 @@
 package com.example.offsetcalculator.rep;
 
 import android.app.Application;
+import android.location.Location;
 import android.util.Log;
 
 import androidx.room.Room;
@@ -11,7 +12,6 @@ import com.example.offsetcalculator.db.AppDatabase;
 import com.example.offsetcalculator.model.route.Coordinate;
 import com.example.offsetcalculator.model.route.Route;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RouteRepository {
@@ -59,6 +59,31 @@ public class RouteRepository {
             Log.d("FAILURE", "Route has no id");
             throw new RuntimeException();
         }
+    }
+
+    public Double calculateDistance(List<Coordinate> coordinates) {
+
+        float total = 0.0f; //holds the total distance betweens pairs of coordinates
+
+        for(int i=0; i < coordinates.size(); i++){
+
+            if(i+1 != coordinates.size()){ //if we are not dealing with last the coordinate of the array
+
+                Coordinate cord = coordinates.get(i);
+                Location loc1 = new Location("Point A");
+                loc1.setLatitude(cord.getLatitude());
+                loc1.setLongitude(cord.getLongitude());
+
+                Coordinate cord2 = coordinates.get(i+1); //could throw an index out of bounds
+                Location loc2 = new Location("Point B");
+                loc2.setLatitude(cord2.getLatitude());
+                loc2.setLongitude(cord2.getLongitude());
+                total = total + loc1.distanceTo(loc2); //use the  distance method in location to get the distance between two points.
+
+            }
+        }
+
+        return (double) total;
     }
 
 }
