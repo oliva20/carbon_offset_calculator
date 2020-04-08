0e326f42992aacd8acebc4c48cc454e720883495
oliva20 <3oliva41@solent.ac.uk>
2020-04-08 16:09:34 +0100

    button changes style when pressed. No more two buttons.

--- app/src/main/java/com/example/offsetcalculator/ui/TransportFragment.java
+++ app/src/main/java/com/example/offsetcalculator/ui/TransportFragment.java
@@ -5,14 +5,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 
 import com.example.offsetcalculator.BuildConfig;
@@ -20,21 +18,19 @@ import com.example.offsetcalculator.R;
 import com.example.offsetcalculator.rep.AirEmissionRepository;
 import com.example.offsetcalculator.rep.BusEmissionRepository;
 import com.example.offsetcalculator.rep.CarEmissionRepository;
-import com.example.offsetcalculator.rep.RouteRepository;
 import com.example.offsetcalculator.services.LocationService;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.views.MapView;
 
 public class TransportFragment extends Fragment implements View.OnClickListener {
+
+    Boolean clicked = false;
     MapView map = null;
     CarEmissionRepository carRep;
     BusEmissionRepository busRep;
     AirEmissionRepository airRep;
-    RouteRepository routeRep;
-    Button btnInsert;
-    Button btnDelete;
-    private Activity activity = getActivity();
+    Button btn;
 
     @Nullable
     @Override
@@ -49,10 +45,8 @@ public class TransportFragment extends Fragment implements View.OnClickListener 
         View view = inflater.inflate(R.layout.fragment_transport, container, false);
 
         //click listeners
-        btnInsert = (Button) view.findViewById(R.id.start_tracking);
-        btnDelete = (Button) view.findViewById(R.id.stop_tracking);
-        btnInsert.setOnClickListener(this);
-        btnDelete.setOnClickListener(this);
+        btn = (Button) view.findViewById(R.id.start_tracking);
+        btn.setOnClickListener(this);
 
         return view;
     }
@@ -60,26 +54,37 @@ public class TransportFragment extends Fragment implements View.OnClickListener 
     //add a boolean that checks if the button has been clicked.
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.start_tracking:
-                Log.d("Button", "Button pressed to start the service");
+
+        Log.d("Clicked", clicked.toString());
+
+        if(v.getId() == R.id.start_tracking){
                 //TODO: add checks for device api differences here.
                 //@@@ This varies between API level. Only use this with 25 or under
-                getActivity().startService(new Intent(getActivity(),LocationService.class));
-                break;
-            case R.id.stop_tracking:
-                Log.d("@@@ Button", "Stop button pressed");
-                getActivity().stopService(new Intent(getActivity(),LocationService.class));
-                break;
+                if(clicked) {
+                    Log.d("@@@ Button", "Location service has stopped");
+                    getActivity().stopService(new Intent(getActivity(),LocationService.class));
+                    clicked = false;
+                    //call the function to change the button style after setting the clicked boolean
+                    changeButtonStyle(clicked);
+                } else {
+                    Log.d("@@@ Button", "Location service has started");
+                    getActivity().startService(new Intent(getActivity(),LocationService.class));
+                    clicked = true;
+                    //call the function to change the button style after setting the clicked boolean
+                    changeButtonStyle(clicked);
+                    Log.d("Clicked", clicked.toString());
+                }
         }
-    }
 
+     }
 
-    //should dismiss keyboard when hitting a button
-    public void dismissKeyboard() {
-        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
-        if (null != activity.getCurrentFocus())
-            imm.hideSoftInputFromWindow(activity.getCurrentFocus()
-                    .getApplicationWindowToken(), 0);
+    private void changeButtonStyle(Boolean isClicked) {
+        if(isClicked) {
+            btn.setText(R.string.btnStopTracking);
+            btn.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        } else {
+            btn.setText(R.string.btnStartTracking);
+            btn.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        }
     }
 }
