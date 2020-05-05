491eff1a3632eac21213b17e8b7ff2ff0429fbda
oliva20 <3oliva41@solent.ac.uk>
2020-05-05 14:15:10 +0100

    added help and about acitivies information

--- app/src/main/java/com/thinkarbon/offsetcalculator/ui/HelpActivity.java
+++ app/src/main/java/com/thinkarbon/offsetcalculator/ui/HelpActivity.java
@@ -1,6 +1,9 @@
 package com.thinkarbon.offsetcalculator.ui;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,11 @@ public class HelpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
         setTitle("Reduce your Emissions");
+        TextView tv = (TextView) findViewById(R.id.meat_text);
+        tv.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView tv2 = (TextView) findViewById(R.id.offset_text);
+        tv2.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
 }
